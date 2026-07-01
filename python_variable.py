642eed13b5bcc83799d85fe66c25aee9cd889fc7
Quantum <mkhum4n@gmail.com>
2026-07-01 17:21:37 +0000

    update1 Varibale file

--- python_variable.py
+++ python_variable.py
@@ -1,2 +1,14 @@
-x = 5
-y = "Hello, World!"
+m = 5
+n = "Hello, World!"
+
+
+#Another Example
+a = 5
+name = "Alex"  
+print(a)
+print(name)
+
+
+#Add more variable
+x, y, z = 1, 2.5, "Python"
+print(x, y, z)
