1224cf44a74cf2de5ade5eaeb1fa333c18e54103
Quantum <mkhum4n@gmail.com>
2026-07-01 17:14:37 +0000

    Another if-else

--- python_identation.py
+++ python_identation.py
@@ -1,2 +1,10 @@
 if 5 > 2:
-  print("Five is greater than two!")
+  print("Five is greater than two")
+
+
+"""Another Example is here"""
+
+if 5 > 2:
+ print("Five is greater than two") 
+if 5 > 2:
+        print("Five is greater than two")
