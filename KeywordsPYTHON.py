b4a5aaf1d7564ceed8607aa29d5b3bd695b80a63
Quantum <mkhum4n@gmail.com>
2026-07-02 06:59:51 +0000

    Major update and added keyword file

---
+++ KeywordsPYTHON.py
@@ -0,0 +1,47 @@
+"""
+A keyword is a reserved word in a programming language that has a predefined meaning and cannot be used for any other purpose, such as naming variables or functions.
+Keywords form the basic building blocks of a program’s syntax.
+"""
+
+
+"""
+A keyword is a reserved word in a programming language that has a predefined meaning and cannot be used for any other purpose, such as naming variables or functions.
+Keywords form the basic building blocks of a program’s syntax.
+"""
+
+
+"""
+Characteristics of Keywords:
+Reserved: Cannot be used as identifiers (variable names, function names, etc.).
+Case-sensitive: In languages like C, C++, Java, and Python, keywords are usually case-sensitive (e.g., if is different from IF).
+Fixed meaning: Keywords have a fixed purpose and meaning defined by the language specification.
+"""
+
+
+"""
+Keywors list of Python: There are total 35 keywords in python and 44 soft keywords
+
+
+Characteristics of Keywords:
+Reserved: Cannot be used as identifiers (variable names, function names, etc.).
+Case-sensitive: In languages like C, C++, Java, and Python, keywords are usually case-sensitive (e.g., if is different from IF).
+Fixed meaning: Keywords have a fixed purpose and meaning defined by the language specification.
+"""
+
+
+"""
+As of the latest versions of Python 3, there are 35 standard reserved keywords and 4 soft keywords. 
+The list of keywords are: 
+'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 
+'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 
+'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 
+'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'
+
+
+Soft keywords only act as reserved words in specific contexts (like pattern matching or type aliasing).
+You can still safely use them as regular variable names elsewhere in your code without causing syntax errors:
+
+match, case, type,  _(underscore)
+
+"""
+
